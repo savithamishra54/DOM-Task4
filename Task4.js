477137ddf0259dd0d296bee8cafaf255f30f772d
savithamishra54 <savithamishra23@gmail.com>
2022-09-20 14:57:19 +0530

    Task4 commit

---
+++ Task4.js
@@ -0,0 +1,26 @@
+var li = document.getElementsByTagName('li');
+
+console.log(li);
+
+console.log(li[1]);
+
+
+
+li[1].style.fontWeight='bold';
+
+li[1].style.backgroundColor='green';
+
+for(var i=0;i<li.length;i++)
+{
+    li[i].style.backgroundColor = '#f4f4f4';
+}
+
+li[2].style.backgroundColor='green';
+
+
+for(var i=0;i<li.length;i++)
+{
+    li[i].style.fontWeight='bold';
+}
+
+ 
